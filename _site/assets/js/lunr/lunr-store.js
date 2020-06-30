ab5b4f8ac40c523568f00cb96b940d3ed535765b
seoJang <jangseo48@gmail.com>
2020-07-01 08:47:25 +0900

    git blog 설정

---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,13 @@
+var store = [{
+        "title": "Welcome to Jekyll!",
+        "excerpt":"You’ll find this post in your _posts directory. Go ahead and edit it and re-build the site to see your changes. You can rebuild the site in many different ways, but the most common way is to run jekyll serve, which launches a web server and auto-regenerates your site when...","categories": ["jekyll","update"],
+        "tags": [],
+        "url": "http://localhost:4000/jekyll/update/welcome-to-jekyll/",
+        "teaser": null
+      },{
+        "title": "Jekyll Test Post",
+        "excerpt":"Blog First Post   jekyll 포스트 업로드 test를 위해 생성한 글입니다.   참고 목록     Jekyll을 이용해 GitHub에 블로그 만들기 (1)   Jekyll을 이용해 GitHub에 블로그 만들기 (2)   Jekyll 공식 홈페이지 ```  ","categories": ["git"],
+        "tags": [],
+        "url": "http://localhost:4000/git/Jekyll-Test/",
+        "teaser": null
+      }]
