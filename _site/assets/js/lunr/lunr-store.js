06543718ddc3f7b333ca10db34060cd79289719d
seoJang <jangseo48@gmail.com>
2020-07-03 15:47:09 +0900

    git blog 0703 AlBJstep04 업로드 및 이전파일 수정

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -5,21 +5,27 @@ var store = [{
         "url": "http://localhost:4000/jekyll/Jekyll-Test/",
         "teaser": null
       },{
-        "title": "BaeckJoon 단계별문제풀이_2557 Hello World",
-        "excerpt":"백준 알고리즘 _단계별로 풀어보기  입출력과 사칙연산  - 2557 문제     - 풀이  Hello World! 를 출력하기   - 코드         - 참고 목록     https://www.acmicpc.net/problem/2557  ","categories": ["BaeckJoon","Algorithm"],
+        "title": "BaeckJoon 단계별_ 입출력과 사칙연산 2557 Hello World",
+        "excerpt":"백준 알고리즘 _단계별로 풀어보기  입출력과 사칙연산  - 2557 문제     풀이  Hello World! 를 출력하기   코드         - 참고 목록     https://www.acmicpc.net/problem/2557  ","categories": ["BaeckJoon"],
         "tags": ["Algorithm","BaeckJoon"],
-        "url": "http://localhost:4000/baeckjoon/algorithm/AlBJstep01/",
+        "url": "http://localhost:4000/baeckjoon/AlBJstep01/",
         "teaser": null
       },{
-        "title": "BaeckJoon 단계별문제풀이_10718 We love kriii",
-        "excerpt":"백준 알고리즘 _단계별로 풀어보기  입출력과 사칙연산  - 10718 문제    - 풀이  글자 출력  - 코드       #### - 참고 목록         https://www.acmicpc.net/problem/2557     ","categories": ["BaeckJoon","Algorithm"],
+        "title": "BaeckJoon 단계별_ 입출력과 사칙연산 10718 We love kriii",
+        "excerpt":"백준 알고리즘 _단계별로 풀어보기  입출력과 사칙연산  - 10718 문제     풀이  글자 출력   코드         #### - 참고 목록     https://www.acmicpc.net/problem/2557  ","categories": ["BaeckJoon"],
         "tags": ["Algorithm","BaeckJoon"],
-        "url": "http://localhost:4000/baeckjoon/algorithm/AlBJstep02/",
+        "url": "http://localhost:4000/baeckjoon/AlBJstep02/",
         "teaser": null
       },{
-        "title": "BaeckJoon 단계별문제풀이 10171, 10172, 1000, 1001, 10998, 1008, 10869, 10430, 2588",
-        "excerpt":"백준 알고리즘 _단계별로 풀어보기  입출력과 사칙연산  - 10171 고양이 문제     코드         - 10172 개 문제     코드         - 1000 A + B 문제     코드         - 1001 A - B 문제     코드    - 10998 A * B 문제     코드         #### - 1008 A / B 문제    코드         - 10869 사칙연산 문제     코드         - 10430 나머지 문제     코드         - 2588 곱셈 문제     코드         - 참고 목록     https://www.acmicpc.net/problem/2557  ","categories": ["BaeckJoon","Algorithm"],
+        "title": "BaeckJoon 단계별_ 입출력과 사칙연산 10171, 10172, 1000, 1001, 10998, 1008, 10869, 10430, 2588",
+        "excerpt":"백준 알고리즘 _단계별로 풀어보기  입출력과 사칙연산  - 10171 고양이 문제     코드          - 10172 개 문제     코드          - 1000 A + B 문제     코드          - 1001 A - B 문제     코드      - 10998 A * B 문제     코드          - 1008 A / B 문제     코드          - 10869 사칙연산 문제     코드          - 10430 나머지 문제     코드          - 2588 곱셈 문제     코드         - 참고 목록     https://www.acmicpc.net/problem/2557  ","categories": ["BaeckJoon"],
         "tags": ["Algorithm","BaeckJoon"],
-        "url": "http://localhost:4000/baeckjoon/algorithm/AlBJstep03/",
+        "url": "http://localhost:4000/baeckjoon/AlBJstep03/",
+        "teaser": null
+      },{
+        "title": "BaeckJoon 단계별_ IF문 단계 1330, 9498, 2753, 14681, 2884",
+        "excerpt":"백준 알고리즘 _단계별로 풀어보기  if문 단계  - 1330 두 수 비교하기 문제     코드          - 9498 시험성적 문제     코드1(if방식)         코드2(switch방식)         코드3(switch방식)          - 2753 윤년 문제     코드          - 14681 사분면 문제     코드          - 2884 알람 시계 문제      코드 1 (Scanner)         코드 2 (BufferedReader - SpringTokenizer)         코드 3 (BufferedReader - split)         - 참고 목록     https://www.acmicpc.net/problem/2557  ","categories": ["BaeckJoon"],
+        "tags": ["Algorithm","BaeckJoon"],
+        "url": "http://localhost:4000/baeckjoon/AlBJstep04/",
         "teaser": null
       }]
