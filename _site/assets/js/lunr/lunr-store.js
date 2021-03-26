93b6c2e6e73eff2d0ba41a4aa0384c7bc17b5c4a
seoJang <jangseo48@gmail.com>
2021-03-26 14:58:51 +0900

    20210326 취약성 수정

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -23,9 +23,87 @@ var store = [{
         "url": "http://localhost:4000/baeckjoon/AlBJstep03/",
         "teaser": null
       },{
-        "title": "BaeckJoon 단계별_ IF문 단계 1330, 9498, 2753, 14681, 2884",
-        "excerpt":"백준 알고리즘 _단계별로 풀어보기  if문 단계  - 1330 두 수 비교하기 문제     코드          - 9498 시험성적 문제     코드1(if방식)         코드2(switch방식)         코드3(switch방식)          - 2753 윤년 문제     코드          - 14681 사분면 문제     코드          - 2884 알람 시계 문제      코드 1 (Scanner)         코드 2 (BufferedReader - SpringTokenizer)         코드 3 (BufferedReader - split)         - 참고 목록     https://www.acmicpc.net/problem/2557  ","categories": ["BaeckJoon"],
+        "title": "BaeckJoon 단계별_ IF문 1330, 9498, 2753, 14681, 2884",
+        "excerpt":"백준 알고리즘 _단계별로 풀어보기  if문  - 1330 두 수 비교하기 문제     코드          - 9498 시험성적 문제     코드1(if방식)         코드2(switch방식)         코드3(switch방식)          - 2753 윤년 문제     코드          - 14681 사분면 문제     코드          - 2884 알람 시계 문제      코드 1 (Scanner)         코드 2 (BufferedReader - SpringTokenizer)         코드 3 (BufferedReader - split)         - 참고 목록     https://www.acmicpc.net/problem/2557  ","categories": ["BaeckJoon"],
         "tags": ["Algorithm","BaeckJoon"],
         "url": "http://localhost:4000/baeckjoon/AlBJstep04/",
+        "teaser": null
+      },{
+        "title": "BaeckJoon 단계별_ For문 2739, 10950, 8393, 15552, 2741, 2742, 11021, 11022, 2438, 2439, 10871",
+        "excerpt":"백준 알고리즘 _단계별로 풀어보기  for문  - 2739 구구단 문제     코드          - 10950 A + B - 3 문제     코드(if방식)          - 8393 합 문제     코드1. Scanner 사용         코드2. BufferedReader-System.out 사용          - 15552 빠른 A + B 문제     코드1. BufferedReader-System.out 사용 : 시간초과(Scanner &amp;System.out.println 사용시 시간초과)         코드2. BufferedReader, split()-BufferedWriter 사용. 1024ms         코드3. BufferedReader, StringTokeniser, BufferedWriter 사용. 744ms          - 2741 N 찍기 문제     코드          - 2742 기찍 N 문제     코드          - 11021 A + B - 7 문제     코드          - 11022 A + B - 8 문제     코드          - 2438 별 찍기- 1 문제     코드          - 2439 별 찍기- 2 문제     코드          - 10871 X보다 작은 수 문제     코드         - 참고 목록     https://www.acmicpc.net/problem/2557  ","categories": ["BaeckJoon"],
+        "tags": ["Algorithm","BaeckJoon"],
+        "url": "http://localhost:4000/baeckjoon/AlBJstep05/",
+        "teaser": null
+      },{
+        "title": "BaeckJoon 단계별(JAVA)_ While문 10952, 10951, 1110",
+        "excerpt":"백준 알고리즘 _단계별로 풀어보기(JAVA)  While문  - 10952 A + B -5 문제     코드1. BufferedWriter 사용 80ms         코드2. StringBuilder 사용 80ms (메모리는 더 적게 사용함)          - 10951 A + B -4 문제 : EOP 해결 문제     코드1. BufferReader (null 반환) + StringTokenizer 80ms         코드2.  BufferReader (null 반환) + charAt() (10이하 정수 값에서 사용 가능) 76ms          - 1110 더하기 사이클 문제     코드         - 참고 목록     https://www.acmicpc.net/problem/2557  ","categories": ["BaeckJoon"],
+        "tags": ["Algorithm","BaeckJoon"],
+        "url": "http://localhost:4000/baeckjoon/AlBJstep06/",
+        "teaser": null
+      },{
+        "title": "BaeckJoon 단계별(JAVA)_ 실습1 10039, 5543, 10817, 2523, 2446, 10996",
+        "excerpt":"백준 알고리즘 _단계별로 풀어보기(JAVA)  실습1  - 10039 평균 점수 문제     코드          - 5543 상근날드 문제     코드1. 배열 하나에 담아서 비교 80ms         코드2. 배열 나눠서 비교 76ms          - 10817 세수 문제     코드1. Array.sort() 사용         코드2. 정렬 사용 안하고 비교          - 2523 별 찍기 -13 문제     코드1. sysout 출력 156ms         코드2. StringBuilder 출력 84ms         코드3. 2번 코드 개선 80ms          - 2446 별 찍기 -9 문제     코드1. 84ms         코드2. 개선          - 10996 별 찍기 -21 문제     코드1. 84ms         - 참고 목록     https://www.acmicpc.net/problem/2557  ","categories": ["BaeckJoon"],
+        "tags": ["Algorithm","BaeckJoon"],
+        "url": "http://localhost:4000/baeckjoon/AlBJstep07/",
+        "teaser": null
+      },{
+        "title": "BaeckJoon 단계별(JAVA)_ 1차원 배열 10818, 2562, 2577, 3052, 1546, 8958, 4344",
+        "excerpt":"백준 알고리즘 _단계별로 풀어보기(JAVA)  1차원 배열  - 10818 최소, 최대     코드1. Math 사용안함 448ms         코드2. Math 사용 412ms      ***    - 2562 최댓값 문제        코드          - 2577 숫자의 개수 문제     코드          - 3052 나머지 문제     코드1. set 사용하지않고 풀기 80ms         코드2. set 사용 80ms          - 1546 평균 문제     코드          - 8958 OX퀴즈 문제     코드1 하나하나 잘라서 비교         코드2 .CharAt() 사용          - 4344 평균은 넘겠지 문제     코드         - 참고 목록     https://www.acmicpc.net/problem/2557  ","categories": ["BaeckJoon"],
+        "tags": ["Algorithm","BaeckJoon"],
+        "url": "http://localhost:4000/baeckjoon/AlBJstep08/",
+        "teaser": null
+      },{
+        "title": "BaeckJoon 단계별(JAVA)_ 함수 15596, 4673, 1065",
+        "excerpt":"백준 알고리즘 _단계별로 풀어보기(JAVA)  함수  - 15596 정수 N개의 합 문제     코드          - 4673 셀프 넘버 문제     코드1. 배열 10001개         코드2. 배열 10000개          - 1065 한수 문제     코드         - 참고 목록     https://www.acmicpc.net/problem/2557  ","categories": ["BaeckJoon"],
+        "tags": ["Algorithm","BaeckJoon"],
+        "url": "http://localhost:4000/baeckjoon/AlBJstep09/",
+        "teaser": null
+      },{
+        "title": "BaeckJoon 단계별(JAVA)_ 문자열 11654, 11720, 10809, 2675, 1157, 1152, 2908, 5622, 2941, 1316",
+        "excerpt":"백준 알고리즘 _단계별로 풀어보기(JAVA)  문자열  - 11654 아스키 코드 문제     코드1. BufferedReader 사용         코드2. System.in.read() 사용          - 11720 숫자의 합 문제     코드          - 10809 알파벳 찾기 문제     코드1. char 비교         코드2. 아스키코드 이용          - 2675 문자열 반복 문제     코드          - 1157 단어 공부 문제     코드1 시간초과         코드2 아스키 코드 208ms         코드3 아스키 코드 + System.in.read() 116ms          - 1152 단어의 개수 문제     코드1 sprit() 사용. 앞뒤공백 읽음/ 공백만 있어도 단어 1개로 계산함.         코드2 StringTokenizer 사용. 앞뒤 공백 무시/ 구분자 기준으로 단어 수만 계산해주는 countTokens() 함수 있음.          - 2908 상수 문제     코드          - 5622 다이얼 문제     코드          - 2941 크로아티아 알파벳 문제  ![image](https://user-images.githubusercontent.com/66898243/87116241-39531400-c2b0-11ea-80ec-e65250aa16b4.png   코드          - 1316 그룹 단어 체커 문제     코드1 이클립스에서는 실행되나 실패         코드2 80ms         - 참고 목록     https://www.acmicpc.net/problem/2557  ","categories": ["BaeckJoon"],
+        "tags": ["Algorithm","BaeckJoon"],
+        "url": "http://localhost:4000/baeckjoon/AlBJstep10/",
+        "teaser": null
+      },{
+        "title": "BaeckJoon 단계별(JAVA)_ 수학1 1712, 2839, 2292, 1193, 2869, 10250, 2775, 1011",
+        "excerpt":"백준 알고리즘 _단계별로 풀어보기(JAVA)  수학 1  - 1712 손익분기점 문제     코드1 이클립스에서는 성공. 백준 실패 111         코드2 76ms          - 2839 설탕 배달 문제     코드1 break설정 안함 실패         코드2 break설정, 반복문 조건 추가          - 2292 벌집 문제     코드 80ms          - 1193 분수찾기 문제     코드1  출력초과         코드2 80ms          - 2869 달팽이는 올라가고 싶다 문제     코드1 시간초과         코드2         코드3          - 10250 ACM 호텔 문제     코드          - 2775 부녀회장이 될테야 문제     코드          - 1011 Fly me to the Alpha Centauri 문제     코드1 시간초과         - 참고 목록     https://www.acmicpc.net/problem/2557  ","categories": ["BaeckJoon"],
+        "tags": ["Algorithm","BaeckJoon"],
+        "url": "http://localhost:4000/baeckjoon/AlBJstep11/",
+        "teaser": null
+      },{
+        "title": "BaeckJoon 단계별(JAVA)_ 수학2 1978, 2581, 1929, 4948, 9020, 1085, 3009, 4153, 3053, 1002",
+        "excerpt":"백준 알고리즘 _단계별로 풀어보기(JAVA)  수학 2  - 1978 소수 찾기 문제     코드          - 2581 소수 문제     코드          - 1929 소수구하기 문제     코드1. 사용 ms      코드2.  사용 ms       - 4948 베르트랑 공준 문제     코드1. 컴파일에러          - 9020 골드하브의 추측 문제     코드       - 1085 직사각형에서 탈출 문제     코드       - 3009 네 번째 점 문제     코드       - 4153 직각삼각형 문제     코드       - 3053 택시 기하학 문제     코드       - 1002 터렛 문제     코드      - 참고 목록         https://www.acmicpc.net/problem/2557     ","categories": ["BaeckJoon"],
+        "tags": ["Algorithm","BaeckJoon"],
+        "url": "http://localhost:4000/baeckjoon/AlBJstep12/",
+        "teaser": null
+      },{
+        "title": "정보처리기사 시험 내용",
+        "excerpt":"정보처리기사/산업기사     개정전 전자계산기(2과목) 과목 제외(13~25)   그외 4과목 기출 내용 비슷함   시나공 + 이기쥬히 + 기출 활용   필기 시험 : 08.22~23 객관식 4지 선택형(과목당 30분)   필기 합격 : 평균 60점이상, 과목당 40점 이상   실기 시험: 10.17~11.01 필답형(2시간 30분)   실기 합격 : 60점이상   1과목 : 소프트웨어 설계     코딩지식위주   2과목 : 소프트웨어 개발     암기위주   3과목 : 데이터베이스 구축     암기위주   기출위주로 출제가 많이 됨   4과목 : 프로그래밍 언어 활용     코딩지식위주   까다로운 문제는 넘어가기(시간분배)   5과목 : 정보시스템 구축 관리     암기위주   순서     3 &gt; 4 &gt; 1/2/5ㄴ     - 참고 목록       정보처리기사 (시나공)  ","categories": ["Certificate"],
+        "tags": ["Certificate","정보처리기사"],
+        "url": "http://localhost:4000/certificate/Certificate/",
+        "teaser": null
+      },{
+        "title": "이클립스 단축키",
+        "excerpt":"이클립스 단축키   Ctrl + D : 줄 삭제 Ctrl + Alt + 위/아래 : 줄 복사 Ctrl + Shift + / : 주석처리 (Ctrl + Shit + \\ 주석 풀기) Ctrl + / : 라인별로 주석처리 Ctrl + Shift + O : import 정리 Ctrl + K : 다음 찾기(Ctrl + Shift + K 이전 찾기)  - 현재 블럭설정된 단어 찾기 Ctrl + H : 검색 다이얼로그(메서드, 타입, 패키지 등 검색 가능) Tab : 들여쓰기 (Shift + Tab : 내어쓰기)   Ctrl + Shift + R : 리소스 찾기 Ctrl + Shift  + Space : 메서드의 괄호안에서 누르면 파라미터 타입 볼 수 있음. Ctrl + 1 : 에러난 곳(빨간 줄 표시)에서 누르면 레이어로 해결방법이 나옴. F3 : 해당 메서드나 클래스가 정의된 곳으로 이동   Ctrl + Shift + L : 단축키 리스트 Alt + Shift + X, R : Run on Server F11 or Ctrl + F11 : 최근 실행한 Run 실행   - 참고     https://blog.outsider.ne.kr/243  ","categories": ["Certificate"],
+        "tags": ["Eclipse"],
+        "url": "http://localhost:4000/certificate/Eclipse-%EB%8B%A8%EC%B6%95%ED%82%A4/",
+        "teaser": null
+      },{
+        "title": "BaeckJoon 단계별(JAVA)_ 재귀 10872, 10870, 2447, 11729",
+        "excerpt":"백준 알고리즘 _단계별로 풀어보기(JAVA)  재귀  - 10872 팩토리얼     코드1 단순반복문 80         코드2 재귀반복문 76         코드3 재귀함수 84          - 10870 피보나치 수 5     코드          - 2447 별찍기 - 10 (별찍기 11과 비슷 https://www.youtube.com/watch?v=WjmEVp-Lgns 참고하기)     코드1 (블로그참고, 실패)         코드2 296ms          - 11729 하노이 탑 이동순서     코드1      - 참고 목록     https://www.acmicpc.net/problem/2557  ","categories": ["BaeckJoon"],
+        "tags": ["Algorithm","BaeckJoon"],
+        "url": "http://localhost:4000/baeckjoon/AlBJstep13/",
+        "teaser": null
+      },{
+        "title": "The Pragmatic Programer _00",
+        "excerpt":"The Pragmatic Programer  The Pragmatic Programer 리뷰&amp;정리   2021.03.16 시작   서문  실용주의 프로그래머 tip     자신의 술에 관심과 애정을 가져라   자신의 일에 대해 생각하면서 일하라  현재의 실천 방법에 대해 검토하는 일회성이 아닌 모든 개발 과정과 일상에서 지속적으로 자신의 일을 분석하고 비평해보는 것   참고  실용주의 프로그래머 http://www.yes24.com/Product/Goods/12501565  ","categories": ["The Pragmatic Programer"],
+        "tags": ["The Pragmatic Programer"],
+        "url": "http://localhost:4000/the%20pragmatic%20programer/ThPragmaticProgramer/",
+        "teaser": null
+      },{
+        "title": "MVC, MVP, MVVM 디자인 패턴 비교",
+        "excerpt":"MVC, MVP, MVVM 디자인 패턴은 왜 필요한가?  프로그램의 크기가 점점 커짐에 따라 코드의 관리가 어렵고 유지보수와 개발효율이 떨어지게 되는데,  이를 개선하기 위해 역할에 따라 코드를 분리시켜 관리하는 프레임워크 패턴이 사용되게 됩니다.   MVC 패턴 :  Model + View + Controller     Model    프로그램에서 사용되는 데이터와 데이터를 처리하는 부분.    View   사용자에게 보여지는 UI 부분. 웹페이지나 모바일어플의 화면.   Controller   사용자의 입력을 받고 처리하는 부분.               사용자가 입력     Controller에서 사용자의 입력을 확인하고 Model 업데이트     Controller에서 Model을 나타낼 View를 선택     View는 Model을 이용하여 화면 출력       MVP 패턴 :  Model + View + Presenter     Model    프로그램에서 사용되는 데이터와 데이터를 처리하는 부분.    View   사용자에게 보여지는 UI 부분. 웹페이지나 모바일어플의 화면.   Presenter    View에서 요청한 정보를 Model로부터 가공해서 View로 전달하는 부분            사용자가 입력     View에서 Presenter에게 데이터를 요청     Presenter에서 Model에게 데이터를 요청     Model은 Presenter에게 데이터를 응답     Presenter는 View에게 데이터를 응답     View는 응답받은 데이터를 이용하여 화면 출력       MVVM 패턴 :  Model + View + ViewModel   참고  https://magi82.github.io/android-mvc-mvp-mvvm/ https://velog.io/@jojo_devstory/%EC%95%88%EB%93%9C%EB%A1%9C%EC%9D%B4%EB%93%9C-%EC%95%84%ED%82%A4%ED%85%8D%EC%B2%98-%ED%8C%A8%ED%84%B4-MVVM%EC%9D%B4-%EB%AD%98%EA%B9%8C https://beomy.tistory.com/43 https://jcchu.medium.com/%EB%94%94%EC%9E%90%EC%9D%B8-%ED%8C%A8%ED%84%B4-mvc-mvp-mvvm-965e324742df https://www.thekpop.net/2020/03/design-pattern-04-mvc-mvp-mvvm-3.html  ","categories": ["etc"],
+        "tags": ["android","mvc","mvp","mvvm"],
+        "url": "http://localhost:4000/etc/DesignPattern/",
         "teaser": null
       }]
